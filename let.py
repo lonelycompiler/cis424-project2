cdb11c69e5e646977d49a3687209a4f494690cd5
Benjamin Ward <90160889+bwardcse@users.noreply.github.com>
2021-10-21 00:08:04 -0400

    real \ float fix

--- let.py
+++ let.py
@@ -80,6 +80,7 @@ def DECL_LIST():
 def DECL():
     global lookahead
     global symbol_table
+    global typ
     id = lookahead
     match(lookahead)
     match(':')
@@ -93,7 +94,7 @@ def DECL():
 def TYPE(v):
     if isinstance(v, int):
         return "int" 
-    elif isinstance(v, real):
+    elif isinstance(v, float):
         return "real"
     else:
         print("Type Error")
@@ -155,8 +156,8 @@ def BASE():
                     print('Error')
                     exit()
                 else: return v
-            elif expr_type == 'real':
-                if(isinstance(v, real) == False):
+            elif typ == 'real':
+                if(isinstance(v, float) == False):
                     print('Error')
                     exit()
                 else: return v
